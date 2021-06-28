6c33aef2d23ac96be42d6a306b2a4555633fcfba
cacadosman <wetmanz23@gmail.com>
2021-06-29 03:41:15 +0700

    initial commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "* * * * * /daily_check.py >> /var/log/cron.log 2>&1
+echo "* */6 * * * /daily_check.py >> /var/log/cron.log 2>&1
 # This extra line makes it a valid cron" > scheduler.txt
 
 crontab scheduler.txt
