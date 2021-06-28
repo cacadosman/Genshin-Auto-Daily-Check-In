6a6c9f9a2c85496375226caf1b17e2e8c8cc582e
cacadosman <wetmanz23@gmail.com>
2021-06-29 03:15:37 +0700

    initial commit

---
+++ daily_check.py
@@ -0,0 +1,47 @@
+#!/usr/local/bin/python3
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+from datetime import datetime
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+driver = webdriver.Chrome(chrome_options=chrome_options)
+
+def set_cookies(driver, raw_cookies):
+    for raw_cookie in raw_cookies.split("; "):
+        cookie = raw_cookie.split("=")
+        driver.add_cookie({
+            'name': cookie[0],
+            'value': cookie[1]
+        })
+
+url = "https://webstatic-sea.mihoyo.com/ys/event/signin-sea/index.html?act_id=e202102251931481&lang=en-us"
+driver.get(url)
+
+cookies = open('cookie.txt', 'r').read().strip()
+set_cookies(driver, cookies)
+
+driver.get(url)
+time.sleep(5)
+
+daily_count_xpath = "//body[1]/div[1]/div[2]/div[1]/div[3]/p[1]/span[1]"
+daily_count = int(driver.find_element_by_xpath(daily_count_xpath).text)
+
+reward_xpath = "//body/div[1]/div[2]/div[1]/div[4]/div[{}]".format(daily_count + 1)
+driver.find_element_by_xpath(reward_xpath).click()
+time.sleep(5)
+
+new_daily_count = int(driver.find_element_by_xpath(daily_count_xpath).text)
+if (new_daily_count > daily_count):
+    print("Success")
+else:
+    print("Already claimed")
+
+driver.close()
+
+report = open('report.txt', 'a+')
+report.write("Script executed at {}".format(datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
+report.close()
---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# UTC Time Zone
+echo "* * * * * /daily_check.py >> /var/log/cron.log 2>&1
+# This extra line makes it a valid cron" > scheduler.txt
+
+crontab scheduler.txt
+cron -f
